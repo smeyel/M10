5e8abf3aef2f9fca9c3089b35948c6d5b266c63a
Kristof <kristof@aut.bme.hu>
2013-07-24 19:44:34 +0200

    M10 and FsmDetectorTest initial commit

---
+++ FsmDetectorTest/include/fsmlearning.h
@@ -0,0 +1,7 @@
+#ifndef __FSMLEARNING_H
+#define __FSMLEARNING_H
+
+void test_graphOpt();
+void test_mkStatFromImageList(const char *offImageFilenameList, const char *onImageFilenameList);
+
+#endif
