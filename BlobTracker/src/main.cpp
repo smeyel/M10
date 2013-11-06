2d3d4b3184b43402580c67f4338051e6fbaeea2e
Kristof <kristof@aut.bme.hu>
2013-11-06 11:33:58 +0100

    Improved main() control state variables
    Removed unnecessary functions from context.

--- BlobTracker/src/main.cpp
+++ BlobTracker/src/main.cpp
@@ -60,8 +60,8 @@ void init_defaults(const char *overrideConfigFileName = NULL)
 
 void exportMeasurementData(TrackingContext &context)
 {
-	cout << "- recalculateLocationConfidences" << endl;
-	context.recalculateLocationConfidences();
+/*	cout << "- recalculateLocationConfidences" << endl;
+	context.recalculateLocationConfidences(); */
 	cout << "- TODO: re-calculate sizeRatioToMean" << endl;
 	cout << "- exportAllDetections, includes path validation" << endl;
 	// exportAllDetections includes path validation
@@ -208,22 +208,30 @@ void test_BlobOnForeground(const char *overrideConfigFileName = NULL)
 
 	frameIdx = -1;
 	bool saveMeasurementDataUponVideoEnd = true;	// Disabled if Esc is pressed
-	enum stateEnum
+
+	enum ProcessingModeEnum
+	{
+		processing,
+		turbo,
+		replay
+	} processingMode = processing;
+
+	enum controlStateEnum
 	{
 		run,
 		pause,
-		turbo,
-		replay,
+		single,
 		finished
-	} state = run;
-	while (state != finished)
+	} controlState = run;
+	while (controlState != finished)
 	{
-		if (state == run || state == turbo || state == replay)
+		if (controlState == run || controlState == single)
 		{
 			if (!camProxy->CaptureImage(0,src))
 			{
 				// No more frames
-				state = finished;
+				cout << "No more input frames." << endl;
+				controlState = finished;
 				break;
 			}
 			frameIdx++;
@@ -231,25 +239,41 @@ void test_BlobOnForeground(const char *overrideConfigFileName = NULL)
 		}
 
 		src->copyTo(*verbose);
-		if (state == run || state == turbo)
+		if (processingMode == processing || processingMode == turbo)
 		{
 			tracker.processFrame(*src,frameIdx,verbose);
 		}
 
 		stringstream oss;
 		oss << "F:" << frameIdx << " ";
-		oss << (state==replay ? "REPLAY" : (state==pause ? "PAUSE" : (state==turbo?"TURBO":"")));
+		if (processingMode == turbo)
+		{
+			oss << " TURBO";
+		}
+		if (processingMode == replay)
+		{
+			oss << " REPLAY";
+		}
+		if (controlState == pause)
+		{
+			oss << " PAUSE";
+		}
 		putText(*verbose,
 			oss.str().c_str(),
 			Point(20, 20), CV_FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255,200,200),1,8,false);
 
-		if (state != turbo)
+		if (processingMode != turbo)
 		{
 			view.show(frameIdx);
 		}
 
+		if (controlState == single)
+		{
+			controlState = pause;
+		}
+
 		char k = -1;
-		if (state != turbo)
+		if (processingMode != turbo)
 		{
 			k = cvWaitKey(25);
 		}
@@ -266,22 +290,41 @@ void test_BlobOnForeground(const char *overrideConfigFileName = NULL)
 		{
 		case -1:	// No keypress
 			break;
+		// --------------- Control state and processing mode
 		case 27:
 			saveMeasurementDataUponVideoEnd = false;
-			state = finished;
+			controlState = finished;
 			break;
 		case 'r':
-			state = run;
+			controlState = run;
 			break;
 		case 'p':
-			state = pause;
+			controlState = pause;
 			break;
 		case 't':
-			state = turbo;
+			processingMode = turbo;
 			break;
-		case '7':
-			state = replay;
+		case 's':
+			controlState = single;
+		// --------------- Switch to replay mode
+		case 'l':	// Load
+			cout << "- loadVehicles" << endl;
+			context.loadVehicles(configmanager.registrationsFilename.c_str());
+			// Motion vectors and sizes not loaded anymore! They are not needed as confidences are already set!
+			//	But do not forget that they are not loaded!
+			// VehicleSizes are also only used for sizeRatioToMean which is already calculated.
+			// TODO: recollect motion vectors and sizes. Confidences already loaded.
+
+			cout << "- loading motion vectors" << endl;
+			context.motionVectorStorage.load(configmanager.motionVectorInputFilename);
+			cout << "- turning off motion vector collection" << endl;
+			context.motionVectorStorage.collectNewMotionVectors = false;
+
+			cout << "- Switching to REPLAY mode" << endl;
+			processingMode = replay;
+			saveMeasurementDataUponVideoEnd = false;
 			break;
+		// --------------- Debug and Verbose functions
 		case 'v':
 			view.configmanager.recordVideo = !view.configmanager.recordVideo;
 			cout << "view.configmanager.recordVideo=" << view.configmanager.recordVideo << endl;
@@ -320,11 +363,6 @@ void test_BlobOnForeground(const char *overrideConfigFileName = NULL)
 			view.configmanager.showPath = !view.configmanager.showPath;
 			cout << "view.configmanager.showPath=" << view.configmanager.showPath << endl;
 			break;
-		case '4':	// toggle showPath
-			camProxy->JumpToFrame(1000);
-			frameIdx = 1000;
-			cout << "Jumping to frame 1000..." << endl;
-			break;
 /*		case 'a':	// Average motion vector length
 			cout << "Mean MotionVector.length() = " << context.motionVectorStorage.getMeanMotionVectorLength() << endl;
 			break; */
@@ -334,9 +372,6 @@ void test_BlobOnForeground(const char *overrideConfigFileName = NULL)
 			break; */
 		case 'i':
 			context.motionVectorStorage.load(configmanager.motionVectorInputFilename);
-			break;
-		case 'I':
-			context.recalculateLocationConfidences();
 			break;
 		// --------------- Export functions -----------------
 		case 'e':
@@ -357,23 +392,6 @@ void test_BlobOnForeground(const char *overrideConfigFileName = NULL)
 			context.saveVehicles(configmanager.registrationsFilename.c_str());
 			cout << "- done" << endl;
 			break; */
-		case '9':
-			cout << "- loadVehicles" << endl;
-			context.loadVehicles(configmanager.registrationsFilename.c_str());
-			// Motion vectors and sizes not loaded anymore! They are not needed as confidences are already set!
-			//	But do not forget that they are not loaded!
-			// VehicleSizes are also only used for sizeRatioToMean which is already calculated.
-			// TODO: recollect motion vectors and sizes. Confidences already loaded.
-
-			cout << "- loading motion vectors" << endl;
-			context.motionVectorStorage.load(configmanager.motionVectorInputFilename);
-			cout << "- turning off motion vector collection" << endl;
-			context.motionVectorStorage.collectNewMotionVectors = false;
-
-			cout << "- Switching to REPLAY mode" << endl;
-			state = replay;
-
-			break;
 		// --------------- Debug functions -----------------
 /*		case 's':	// Show mean size
 			view.verboseMeanSizeAtLastClickLocation();
@@ -385,25 +403,13 @@ void test_BlobOnForeground(const char *overrideConfigFileName = NULL)
 			cout
 				<< "--- run control functions ---" << endl
 				<< "r	Run mode" << endl
-				<< "p	Pause mode" << endl
-				<< "t	Turbo mode" << endl
-				<< "v	Toggle Video recording (output video)" << endl
+				<< "p	Pause mode (no frame capture, no processing)" << endl
+				<< "t	Turbo processing mode (no visualization)" << endl
+				<< "s	Single step" << endl
+				<< "l	Load previous data for replay mode" << endl
 				<< "Esc	Exit program" << endl
-				<< "--- data manipulation functions ---" << endl
-				<< "c	Clear TrackedVehicle, MotionVector and VehicleSize storages" << endl
-				<< "m	Collect motion vectors and re-calculate LocationRegistration confidences" << endl
-				<< "1	Collect only confident motion vectors and re-calculate LocationRegistration confidences" << endl
-				<< "--- visualization functions ---" << endl
-				<< "2	Toggle doSaveImages (override ini)" << endl
-				<< "3	Toggle showPath" << endl
-				<< "a	Show average motion vector length" << endl
-				<< "--- MotionVector persistance functions ---" << endl
-				<< "M	Save motion vectors (filename defined by ini)" << endl
-				<< "i	Import motion vectors and re-calculate LocationRegistration confidences" << endl
-				<< "--- Export functions ---" << endl
-				<< "e	Export all detection data" << endl
 				<< "--- Debug functions ---" << endl
-				<< "s	Show mean sizes at lastMouseClickLocation" << endl;
+				<< "q	Query mode" << endl;
 			break;
 		}
 	}
